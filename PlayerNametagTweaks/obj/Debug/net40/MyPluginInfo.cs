d3ee32b33d42e1b67c7cf7e6c9e5f2e797313652
Michał Czemierowski <michalc.jt@gmail.com>
2024-08-31 18:51:01 +0200

    remove all references to old project name

---
+++ PlayerNametagTweaks/obj/Debug/net40/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace PlayerNametagTweaks
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "PlayerNametagTweaks";
+        public const string PLUGIN_NAME = "PlayerNametagTweaks";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
--- PlayerNametagTweaks/obj/Release/net35/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace NametagMod
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "NametagMod";
-        public const string PLUGIN_NAME = "NametagMod";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
